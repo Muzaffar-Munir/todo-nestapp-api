2f27094df564994e55972dc0c9c3dabacfe3735a
muzaffar.munir <muzaffar.munir@virtual-force.com>
2023-03-10 11:07:32 +0500

    roles for admin

--- src/modules/specializations/specializations.controller.ts
+++ src/modules/specializations/specializations.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { ERoles } from 'src/common/enum/role';
 import { Roles } from 'src/role-guard/roles.decorator';
 import { RolesGuard } from 'src/role-guard/roles.guard';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
@@ -6,19 +7,18 @@ import { CreateSpecializationDto } from './specializations.dto';
 import { SpecializationService } from './specializations.service';
 
 @Controller('specialization')
-@UseGuards(JwtAuthGuard)
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class SpecializationsController {
   constructor(private readonly specializationService: SpecializationService) {}
 
   @Get()
-  @UseGuards(RolesGuard)
-  @Roles('admin')
+  @Roles(ERoles.ADMIN)
   getListings() {
     return this.specializationService.findAll();
   }
 
   @Post()
-  @Roles('admin')
+  @Roles(ERoles.ADMIN)
   createSpecialization(@Body() body: CreateSpecializationDto) {
     return this.specializationService.createSpecialization(body);
   }
